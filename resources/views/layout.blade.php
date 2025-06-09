a4b100d2e08b306c19eca1ab758f48ba567d813e
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 13:33:31 -0300

    Vesion: 005.024.001.000

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,17 @@
+@php
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Foundation\Vite as ViteFoundation;
+
+$viteDataSchemeLight = new ViteFoundation();
+$viteDataSchemeLight->useStyleTagAttributes([
+'data-scheme' => 'light',
+]);
+
+$viteDataSchemeDark = new ViteFoundation();
+$viteDataSchemeDark->useStyleTagAttributes([
+'data-scheme' => 'dark',
+]);
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +19,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" href="{{ asset('/vendor/horizon/img/favicon.png') }}">
+    <link rel="shortcut icon" href="{{ Vite::asset('resources/img/favicon.png', 'vendor/horizon') }}">
 
     <title>Horizon{{ config('app.name') ? ' - ' . config('app.name') : '' }}</title>
 
     <!-- Style sheets-->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:300,400,500,600" rel="stylesheet" />
-    <link href="{{ asset(mix('app.css', 'vendor/horizon')) }}" rel="stylesheet" data-scheme="light">
-    <link href="{{ asset(mix('app-dark.css', 'vendor/horizon')) }}" rel="stylesheet" data-scheme="dark">
+
+    {{ $viteDataSchemeLight('resources/sass/styles.scss', 'vendor/horizon') }}
+    {{ $viteDataSchemeDark('resources/sass/styles-dark.scss', 'vendor/horizon') }}
 </head>
 <body>
 <div id="horizon" v-cloak>
@@ -141,6 +156,6 @@
     window.Horizon = @json($horizonScriptVariables);
 </script>
 
-<script src="{{asset(mix('app.js', 'vendor/horizon'))}}"></script>
+@vite('resources/js/app.js', 'vendor/horizon')
 </body>
 </html>
